ed213d1f6e7dac75f37f1798276f953f5556eb97
wualtervera <waltervera693@gmail.com>
2021-10-30 20:15:47 -0500

    remove bootstrap

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,38 +1,23 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
         'type' => 'proyect',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => NULL,
-        'name' => 'devcreativa/sube-pe',
+        'reference' => '86959e815d7cbda31adcba9c85e7dcefa8af637c',
+        'name' => 'wualtervera/php-mvc',
         'dev' => true,
     ),
     'versions' => array(
-        'devcreativa/sube-pe' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
+        'wualtervera/php-mvc' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
             'type' => 'proyect',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => NULL,
+            'reference' => '86959e815d7cbda31adcba9c85e7dcefa8af637c',
             'dev_requirement' => false,
-        ),
-        'twbs/bootstrap' => array(
-            'pretty_version' => 'v5.1.3',
-            'version' => '5.1.3.0',
-            'type' => 'library',
-            'install_path' => __DIR__ . '/../twbs/bootstrap',
-            'aliases' => array(),
-            'reference' => '1a6fdfae6be09b09eaced8f0e442ca6f7680a61e',
-            'dev_requirement' => false,
-        ),
-        'twitter/bootstrap' => array(
-            'dev_requirement' => false,
-            'replaced' => array(
-                0 => 'v5.1.3',
-            ),
         ),
     ),
 );
